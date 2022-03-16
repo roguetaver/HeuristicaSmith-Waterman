3ff8b398c89003a867b6bbe54752f8b704a1e1fa
roguetaver <andretavernaro@gmail.com>
2022-03-15 21:19:52 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# SuperCompProjeto
-SuperCompProjeto
+# Enunciados dos projetos de Supercomputação do 1o. Semestre/2022
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "test" << endl;
+
+    return 0;
+}
+
+// g++ -Wall -O3 main.cpp -o entrega1
+// ./entrega1 < test.txt
